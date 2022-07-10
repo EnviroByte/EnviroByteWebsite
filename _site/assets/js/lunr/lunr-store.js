3b164d11fc123f01691b0a2e15415a14167b63a9
Minxing <minxingsi@gmail.com>
2022-07-09 21:15:50 -0600

    update ruby 3.1.1

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "PEMS for a Cogeneration Unit",
+        "excerpt":"The study provides an overview of Predictive Emissions Monitoring System’s (PEMS) research, application, installation, and regulatory framework as well as develops predictive models for NOx emissions from a natural gas fired cogeneration unit using an open source machine learning library, Keras, and open source programming languages, Python and R. Nine...","categories": [],
+        "tags": ["PEMS","NOx monitoting","TensorFlow","Neural Network"],
+        "url": "http://localhost:4000/datascience/PEMS_Cogen/",
+        "teaser": null
+      },{
+        "title": "Evaluation and calibration of a low-cost particle sensor",
+        "excerpt":"Particle sensing technology has shown great potential for monitoring particulate matter (PM) with very few temporal and spatial restrictions because of its low cost, compact size, and easy operation. However, the performance of low-cost sensors for PM monitoring in ambient conditions has not been thoroughly evaluated. Monitoring results by low-cost...","categories": [],
+        "tags": ["Air quality monitoring","PM2.5 monitoting","Machine Learning","Neural Network","XGBoost"],
+        "url": "http://localhost:4000/datascience/PM_Sensor_Calibration/",
+        "teaser": null
+      },{
+        "title": "Combined Monthly Petrinex Data into One File",
+        "excerpt":"Petrinex is a data warehouse https://www.petrinex.ca/PD/Pages/default.aspx. It has monthly volumetric data from Oil and Gas operations in Alberta, such as production, fuel use, flare, vent data. The volumetric data can be here https://www.petrinex.ca/PD/Pages/APD.aspx. Note: data for Saskatchewan and British Columbia are not publicly available. Check all the monthly files import...","categories": ["Data Science"],
+        "tags": ["Petrinex","Data Science","Oil and Gas"],
+        "url": "http://localhost:4000/data%20science/combined-data-from-Petrinex/",
+        "teaser": null
+      },{
+        "title": "Emission Value Correction",
+        "excerpt":"The equations are taken from Continuous Emission Monitoring by James A. Jahnke (April 3, 2000). Correct wet-basis concentrations to dry-basis concentrations \\[c_d = \\frac{c_w}{1-m_{Flue Gas}}\\] where \\(c_d\\) = dry-basis concentration value (ppm or percent) \\(c_w\\) = wet-basis concentration value (ppm or percent) \\(m_{Flue Gas}\\) = moisture fraction of the flue...","categories": ["CEMS"],
+        "tags": ["Emissions Correction"],
+        "url": "http://localhost:4000/cems/emission-value-correction/",
+        "teaser": null
+      },{
+        "title": "CEMS Extractive System",
+        "excerpt":"Extractive systems were the first type of CEMS developed for continuous emissions monitoring. Two types of extractive systems fully extractive, source-level systems dilution systems. There are three types of source-level extractive systems: hot/wet system. it uses a heated line to transport the flue gas to an analyzer that incorporates a...","categories": ["CEMS"],
+        "tags": ["CEMS","Emissions monitoring"],
+        "url": "http://localhost:4000/cems/CEMS-Extractive-system/",
+        "teaser": null
+      },{
+        "title": "PEMS for a Cogeneration Unit",
+        "excerpt":"The study provides an overview of Predictive Emissions Monitoring System’s (PEMS) research, application, installation, and regulatory framework as well as develops predictive models for NOx emissions from a natural gas fired cogeneration unit using an open source machine learning library, Keras, and open source programming languages, Python and R. Nine...","categories": [],
+        "tags": ["PEMS","NOx monitoting","TensorFlow","Neural Network"],
+        "url": "http://localhost:4000/regulatory/PEMS_Cogen/",
+        "teaser": null
+      },{
+        "title": "Evaluation and calibration of a low-cost particle sensor",
+        "excerpt":"Particle sensing technology has shown great potential for monitoring particulate matter (PM) with very few temporal and spatial restrictions because of its low cost, compact size, and easy operation. However, the performance of low-cost sensors for PM monitoring in ambient conditions has not been thoroughly evaluated. Monitoring results by low-cost...","categories": [],
+        "tags": ["Air quality monitoring","PM2.5 monitoting","Machine Learning","Neural Network","XGBoost"],
+        "url": "http://localhost:4000/regulatory/PM_Sensor_Calibration/",
+        "teaser": null
+      },{
+        "title": "ISO Adaptation to Climate Change",
+        "excerpt":"   ISO 14090:2019 Adaptation to climate change — Principles, requirements and guidelines   ISO/DIS 14091 Adaptation to climate change — Guidelines on vulnerability, impacts and risk assessment   ISO/TS 14092:2020 Adaptation to climate change — Requirements and guidance on adaptation planning for local governments and communities   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/sustainability/ISO_Adaptation_Climate_Change/",
+        "teaser": null
+      }]
